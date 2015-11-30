649f0bc8e0a7cd31794e38ea3b5e2d9eed531f3e
kennibc <kennibc@dearbornschools.org>
2015-11-30 12:42:31 -0500

    version 1.2.3

--- renderers/course_renderer.php
+++ renderers/course_renderer.php
@@ -31,7 +31,7 @@ class theme_pioneer_core_course_renderer extends core_course_renderer {
 
     public function frontpage_available_courses() {
         /* available courses */
-        global $CFG, $OUTPUT;
+        global $CFG, $OUTPUT, $PAGE;
         require_once($CFG->libdir. '/coursecatlib.php');
 
         $chelper = new coursecat_helper();
@@ -70,10 +70,8 @@ class theme_pioneer_core_course_renderer extends core_course_renderer {
 
                     $no = get_config('theme_pioneer', 'patternselect');
                     $nimgp = (empty($no)||$no == "default") ? 'no-image' : 'cs0'.$no.'/no-image';
-
                     $noimgurl = $OUTPUT->pix_url($nimgp, 'theme');
                     $courseurl = new moodle_url('/course/view.php', array('id' => $courseid ));
-
                     if ($course instanceof stdClass) {
                         require_once($CFG->libdir. '/coursecatlib.php');
                         $course = new course_in_list($course);
@@ -93,7 +91,7 @@ class theme_pioneer_core_course_renderer extends core_course_renderer {
                     }
 
                     if (empty($imgurl)) {
-                        $imgurl = $noimgurl;
+                        $imgurl = $PAGE->theme->setting_file_url('headerbackgroundimage', 'headerbackgroundimage', true);
                     }
 
                     $rowcontent .= '<div class="span3">
@@ -124,7 +122,7 @@ class theme_pioneer_core_course_renderer extends core_course_renderer {
     }
 
     public function promoted_courses() {
-        global $CFG, $OUTPUT, $DB;
+        global $CFG, $OUTPUT, $DB, $PAGE;
 
         $pcourseenable = theme_pioneer_get_setting('pcourseenable');
         if (!$pcourseenable) {
@@ -215,7 +213,7 @@ class theme_pioneer_core_course_renderer extends core_course_renderer {
                     }
 
                     if (empty($imgurl)) {
-                        $imgurl = $noimgurl;
+                        $imgurl = $PAGE->theme->setting_file_url('headerbackgroundimage', 'headerbackgroundimage', true);
                     }
 
                         $coursehtml = '<div class="span2">
